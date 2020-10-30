502d862d855cf07abaa2bede2d8d66edb05e5eb7
Will Studabaker <will.studabaker@gmail.com>
2020-10-30 15:01:33 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
+#{"This, well, is a sentence. This is too!! And so is this, I think? Woo..."}
   def count_sentences
-
+    sent_split = self.split(/[.?!]+/)
+#  binding.pry
+    sent_split.length
   end
 end
